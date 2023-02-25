ac184d2d0a82075222410bb0d2bd130b99661379
Daniel Falk <danielfalk22@gmail.com>
2023-02-25 14:27:19 -0500

    Move setup to main module
    This should allow a different way of configuration that some plugins
    seem to allow: require("smart-open").setup(...)

---
+++ lua/smart-open/init.lua
@@ -0,0 +1,26 @@
+local DbClient = require("telescope._extensions.smart_open.dbclient")
+local default_config = require("telescope._extensions.smart_open.default_config")
+local history = require("telescope._extensions.smart_open.history")
+
+local config = vim.tbl_deep_extend("force", {}, default_config)
+
+local function set_config(opt_name, value)
+  if value ~= nil then
+    config[opt_name] = value
+  end
+end
+
+return {
+  config = config,
+  setup = function(ext_config)
+    set_config("show_scores", ext_config.show_scores)
+    set_config("disable_devicons", ext_config.disable_devicons)
+    set_config("ignore_patterns", ext_config.ignore_patterns)
+    set_config("match_algorithm", ext_config.match_algorithm)
+
+    config.db_filename = vim.fn.stdpath("data") .. "/smart_open.sqlite3"
+
+    local db = DbClient:new({ path = config.db_filename })
+    history:setup(db, config)
+  end,
+}
